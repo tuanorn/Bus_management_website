86f2e20f81de45562927abd06db640d0833673b9
tuanorn <tuanminhfor3@gmail.com>
2024-07-19 08:02:48 -0400

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Header';
+import Home from './Home';
+import Account from './Account';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <>
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/account" element={<Account/>} />
+            </Routes>
+        </BrowserRouter>
+    </>
 }
 
 export default App;
